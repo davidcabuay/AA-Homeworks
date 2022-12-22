1e70e0eb751067b7f877f4a19390f8d9590e65a9
David <david.cabuay@gmail.com>
2022-12-21 19:31:10 -0800

    Started W3D4 homework

---
+++ octopus_problems.rb
@@ -0,0 +1,32 @@
+fishes = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 'ffiiiiisshh', 'fsh', 
+ 'fiiiissshhhhhh']
+
+ #sluggish octopus
+
+ def sluggish_octopus(arr)
+    longest_fish = ""
+    arr.each do |fish1|
+        arr.each do |fish2|
+            if fish1.length < fish2.length
+                longest_fish = fish2
+            else
+                longest_fish = fish1
+            end
+        end
+    end
+    longest_fish
+ end
+#  p sluggish_octopus(fishes)
+
+ def dominant_octopus(arr)
+    return self if arr.length <=1
+    pivot = arr[0]
+    left = arr[1..-1].select{|el| el.length < pivot.length}
+    right = arr[1..-1].select{|el| el.length >= pivot.length}
+    result = left.dominant_octopus(arr) + [pivot] + right.dominant_octopus(arr)
+    return result[-1]
+ end
+
+#  p dominant_octopus(fishes)
+
+ def clever_octopus
